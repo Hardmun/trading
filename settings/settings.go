1e680184f35f34fa9136a97733471ae2cd61a2b3
Hardmoon <spamnvg@gmail.cm>
2024-10-24 18:59:28 +0300

    HQ

--- settings/settings.go
+++ settings/settings.go
@@ -21,30 +21,41 @@ type Limiter struct {
 	countLimit int
 	count      int
 	ticker     *time.Ticker
+	Error      chan error
 }
 
 //go:embed autostart.sql
 var EmbedFiles embed.FS
-var Limits = NewLimiter(2*time.Second, 2)
+var Limits = NewLimiter(2*time.Second, 4)
 
 func NewLimiter(d time.Duration, c int) *Limiter {
 	limiter := &Limiter{
 		countLimit: c,
 		count:      c,
 		ticker:     time.NewTicker(d),
+		Error:      make(chan error, 1),
 	}
 	return limiter
 }
 
-func (l *Limiter) Wait() {
+func (l *Limiter) Wait() error {
+	select {
+	case err := <-l.Error:
+		return err
+	default:
+	}
+
 	select {
 	case <-l.ticker.C:
 		l.count = l.countLimit
 	default:
 	}
+
 	if l.count <= 0 {
 		<-l.ticker.C
 		l.count = l.countLimit
 	}
 	l.count--
+
+	return nil
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,19 +1,45 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 	"trading/data"
 	"trading/settings"
 )
 
-func writeDataIfNotExists(symbol, interval string, startTime, endTime int64) {
+var (
+	//wg = sync.WaitGroup{}
+	w = 0
+)
+
+func writeDataIfNotExists(symbol, interval string, startTime, endTime int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	req, err := http.NewRequest(http.MethodGet, "https://api.binance.com/api/v3/klines", nil)
+	if err != nil {
+		settings.Limits.Error <- err
+		_ = req
+	}
+	fmt.Println(symbol)
+	if w == 2 {
+		settings.Limits.Error <- errors.New("LOL")
+		return
+	}
+	w++
+
+	time.Sleep(5 * time.Second)
+	fmt.Printf("%v  %v\n", time.UnixMilli(startTime).UTC(), time.UnixMilli(endTime).UTC())
 
 }
 
-func UpdateTables() error {
+func UpdateTables() (err error) {
 	minTime := time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+	var wg = new(sync.WaitGroup)
 
+lb:
 	for _, symbol := range settings.Symbols {
 		for interval, timeInt := range settings.Intervals {
 			currentTime := time.Now().Truncate(timeInt).UnixMilli()
@@ -21,17 +47,26 @@ func UpdateTables() error {
 
 			for timeStart, timeEnd := currentTime-step, currentTime-int64(time.Nanosecond); timeEnd >
 				minTime; timeStart, timeEnd = timeStart-step, timeEnd-step {
-				//settings.Limits.Wait()
-
-				writeDataIfNotExists(symbol, interval, timeStart, timeEnd)
-				fmt.Printf("%v  %v\n", time.UnixMilli(timeStart).UTC(), time.UnixMilli(timeEnd).UTC())
+				err = settings.Limits.Wait()
+				if err != nil {
+					break lb
+				}
+				wg.Add(1)
+				go writeDataIfNotExists(symbol, interval, timeStart, timeEnd, wg)
 			}
 		}
-
+	}
+	for len(settings.Limits.Error) > 0 {
+		err = <-settings.Limits.Error
+	}
+	wg.Wait()
+	select {
+	case err = <-settings.Limits.Error:
+	default:
 	}
 
 	db := data.DB
 
 	_ = db
-	return nil
+	return
 }
