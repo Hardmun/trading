b8fbb19d18d25ce1baf3b0dc31bbd1a311a05a1d
Hardmoon <spamnvg@gmail.cm>
2024-10-25 00:26:24 +0300

    hq

--- utils/utils.go
+++ utils/utils.go
@@ -1,49 +1,36 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"sync"
 	"time"
-	"trading/data"
 	"trading/settings"
 )
 
 type klineParams struct {
-	symbol      string
-	interval    string
-	timeStart   int64
-	timeEnd     int64
-	wg          *sync.WaitGroup
-	limiter     *settings.Limiter
-	errMessages *settings.ErrorMessages
+	symbol    string
+	interval  string
+	timeStart int64
+	timeEnd   int64
 }
 
-func writeDataIfNotExists(params klineParams) {
-	defer params.wg.Done()
-
+func updateKlineData(params klineParams) error {
 	req, err := http.NewRequest(http.MethodGet, "https://api.binance.com/api/v3/klines", nil)
 	if err != nil {
-		params.errMessages.WriteError(err)
 		_ = req
-	}
+		return err
 
-	fmt.Println(params.symbol)
-	if params.timeStart == 1643328000000 {
-		params.errMessages.WriteError(errors.New("LOL"))
-		return
 	}
-
-	time.Sleep(5 * time.Second)
 	fmt.Printf("%v  %v\n", time.UnixMilli(params.timeStart).UTC(), time.UnixMilli(params.timeEnd).UTC())
 
+	return nil
 }
 
 func UpdateTables() error {
 	limiter := settings.NewLimiter(2*time.Second, 2)
-	wg := new(sync.WaitGroup)
-	errMessage := settings.NewErrorMessage()
+	wGrp := new(sync.WaitGroup)
+	errMsg := settings.NewErrorMessage()
 
 	minTime := time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
 
@@ -56,34 +43,34 @@ lb:
 			for timeStart, timeEnd := currentTime-step, currentTime-int64(time.Nanosecond); timeEnd >
 				minTime; timeStart, timeEnd = timeStart-step, timeEnd-step {
 
-				if errMessage.HasError() {
+				if errMsg.HasError() {
 					fmt.Printf("breaking on: %s\n", symbol)
 					break lb
 				}
 
 				limiter.Wait()
-				wg.Add(1)
-				params := klineParams{
-					symbol:      symbol,
-					interval:    interval,
-					timeStart:   timeStart,
-					timeEnd:     timeEnd,
-					wg:          wg,
-					limiter:     limiter,
-					errMessages: errMessage,
+				wGrp.Add(1)
+
+				klParams := klineParams{
+					symbol:    symbol,
+					interval:  interval,
+					timeStart: timeStart,
+					timeEnd:   timeEnd,
 				}
-				go writeDataIfNotExists(params)
+				go func(params klineParams, wg *sync.WaitGroup, errMessages *settings.ErrorMessages) {
+					defer wg.Done()
+					err := updateKlineData(params)
+					if err != nil {
+						errMessages.WriteError(err)
+					}
+				}(klParams, wGrp, errMsg)
 			}
 		}
 	}
-	wg.Wait()
+	wGrp.Wait()
 
-	if err := errMessage.GetError(); err != nil {
+	if err := errMsg.GetError(); err != nil {
 		return err
 	}
-
-	db := data.DB
-
-	_ = db
 	return nil
 }
