ffc28ef045ce279d65235821b0c6e307c711b0d1
Hardmoon <spamnvg@gmail.cm>
2024-10-30 15:35:57 +0300

    HQ

--- test/dbspeed_test.go
+++ test/dbspeed_test.go
@@ -27,11 +27,12 @@ var Record = []any{
 	"112059089.48321300",
 }
 
+// Batch writing
 func BatchWriting(step int, startDate int64) {
 	defer wg.Done()
 	queryText := strings.Replace(queries.InsertTradingData, "&tableName",
 		"BTCUSDT_1h", 1)
-	for i := 0; i <= step-1; i++ {
+	for i := 0; i < step; i++ {
 		NewRecord := Record
 		NewRecord[0] = startDate
 		startDate += int64(time.Second / time.Millisecond)
@@ -46,12 +47,12 @@ func BatchWriting(step int, startDate int64) {
 func PrepareBatchWriting() error {
 	startDate := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
 	step := config.Step
-	for t := 0; t < 2000; t, startDate = t+1, startDate+int64(step)*int64(time.Second/time.Millisecond) {
+	for t := 0; t < 1; t, startDate = t+1, startDate+int64(step)*int64(time.Second/time.Millisecond) {
 		if errMessage.HasError() {
 			break
 		}
 		wg.Add(1)
-		go BatchWriting(500, startDate)
+		go BatchWriting(step, startDate)
 	}
 
 	wg.Wait()
@@ -64,7 +65,7 @@ func PrepareBatchWriting() error {
 	return nil
 }
 
-func Test(t *testing.T) {
+func TestBatchWriting(t *testing.T) {
 	errMessage = utils.GetErrorMessage()
 	t.Run("DB connection and table updating", func(t *testing.T) {
 		var err error
@@ -84,4 +85,56 @@ func Test(t *testing.T) {
 			t.Fatal(err)
 		}
 	})
+}
+
+func BenchmarkBatchWriting(b *testing.B) {
+	startDate := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+	step := config.Step
+	_, err := sqlite.GetDb()
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = sqlite.UpdateDatabaseTables()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		startDate += startDate + int64(step)*int64(time.Second/time.Millisecond)
+		BatchWriting(step, startDate)
+		wg.Wait()
+	}
+}
+
+// Sync writing
+func TestSyncWriting(t *testing.T) {
+	t.Run("DB connection and table updating", func(t *testing.T) {
+		var err error
+		_, err = sqlite.GetDb()
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = sqlite.UpdateDatabaseTables()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("Writing messages to database", func(t *testing.T) {
+		queryText := strings.Replace(queries.InsertTradingData, "&tableName",
+			"BTCUSDT_1h", 1)
+		startDate := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+
+		for i := 0; i < 1000000; i++ {
+			NewRecord := Record
+			NewRecord[0] = startDate
+			startDate += int64(time.Second / time.Millisecond)
+
+			err := sqlite.ExecQuery(queryText, NewRecord...)
+			if err != nil {
+				errMessage.WriteError(err)
+			}
+		}
+	})
+
 }
